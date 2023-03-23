a5a3db8e1d46e9f749bb3d734fb4a535afc64268
AULA-208-10 <federicourichlc@gmail.com>
2023-03-23 20:19:46 -0300

    sixthy commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-console.log('hola mundo');
+console.log('hola hola');
 const { request, response } = require('express');//por convencion le ponemos el mismop nombre
 const express = require('express');
 
 const app= express();
 app.get('/api',(req,res)=>{console.log('paso por api');
-res.json({mensaje:'hola mundo', estado:'OK'}).status(200)});
-app.listen(3000,() =>{console.log('escuchando en el puerto 3000')});
+res.json({mensaje:'hola mundo', estado:'OKi'}).status(200)});
+app.listen(3001,() =>{console.log('escuchando en el puerto 3000')});
 
 /*Windows PowerShell
 Copyright (C) Microsoft Corporation. Todos los derechos reservados.
